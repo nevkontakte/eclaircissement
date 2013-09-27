24e1a08aa86b760b1b1240507fae08ba24e48034
Aleks Nevkontakte <aleks@aradmin.org.ru>
2013-09-27 12:16:35 +0700

    UPD: Final version of lab3

--- ruby/3/lab3.rb
+++ ruby/3/lab3.rb
@@ -1,8 +1,9 @@
 lines = readlines.map { |line|
+	# Parse each line as regexp
 	line.match(/
 		^
-		(\d{4}\/\d{2}\/\d{2})\s+ # Date
-		(\d{2}:\d{2}:\d{2})\s+ # Time
+		((\d{4})\/(\d{2})\/(\d{2}))\s+ # Date
+		((\d{2}):(\d{2}):(\d{2}))\s+ # Time
 		(\[\w+\])\s+ # Tag
 		(\d+)\# # Error code no 1?
 		(\d+):\s+ # Error code no 2?
@@ -17,26 +18,42 @@ lines = readlines.map { |line|
 		$
 	/x)
 }.select{|i| i != nil}.map { |line|
+	# Convert metch result into a hash
 	line = line.to_a
-	_, date, time, tag, errno1, errno2, message = line
-	[date, time, tag, errno1, errno2, message]
+	_, date, year, month, day, time, hour, min, sec, tag, errno1, errno2, message = line
+	
 	data = {
 		:date => date,
+		:year => year,
+		:month => month,
+		:day => day,
 		:time => time,
+		:hour => hour,
+		:min => min,
+		:sec => sec,
 		:tag => tag,
 		:errno1 => errno1,
 		:errno2 => errno2,
 		:message => message
 	}
-	rest = line.to_a[data.size+1..line.size]
-	
+	rest = line.to_a[data.size+1..line.size].each_slice(2).to_a
+	data.merge Hash[rest]
+}
+puts lines[1..3], "\n"
+
+# Hits per hour
+puts lines.reduce({}) { |hits, record|
+	date = record[:date] + " " + record[:hour]
+	hits[date] =  hits[date].to_i + 1
+
+	hits
+}, "\n"
+
+# Error types
+puts lines.reduce({}) { |hits, record|
+	errno = record[:errno1] + " " + record[:errno2]
+	hits[errno] =  hits[errno].to_i + 1
+
+	hits
 }
 
-# puts lines.reduce({}) { |hits, line|
-# 	date = line[1]
-# 	hits[date] =  hits[date].to_i + 1
-
-# 	hits
-# }
-
-puts lines
