b5e85c3c0089650d4fd01db1f70398fc881c3595
Aleks Nevkontakte <aleks@aradmin.org.ru>
2013-10-04 15:56:34 +0700

    ADD: Lab5 initial commit

---
+++ clojure/5/src/lab5/core.clj
@@ -0,0 +1,1 @@
+(ns lab5.core)
---
+++ clojure/5/test/lab5/test/core.clj
@@ -0,0 +1,6 @@
+(ns lab5.test.core
+  (:use [lab5.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
