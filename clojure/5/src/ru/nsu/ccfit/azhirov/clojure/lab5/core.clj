eb555b36de6e93b403775b0681005d0436cabd49
Aleks Nevkontakte <aleks@aradmin.org.ru>
2013-10-04 19:42:33 +0700

    ADD: Lab5 completed

--- clojure/5/src/ru/nsu/ccfit/azhirov/clojure/lab5/core.clj
+++ clojure/5/src/ru/nsu/ccfit/azhirov/clojure/lab5/core.clj
@@ -1,1 +1,36 @@
 (ns ru.nsu.ccfit.azhirov.clojure.lab5.core)
+
+
+(defn trapeze [f x1 x2]
+  (*
+    (/
+      (+ (f x1) (f x2))
+      2)
+    (Math/abs (- x2 x1))))
+
+(defn int_step [myself, f, step, delta]
+  (if (<= step 0)
+    0
+    (let [x2 (* step delta)
+          x1 (- x2 delta)]
+      (+
+        (myself myself f (dec step) delta)
+        (trapeze f x1 x2)
+        ))
+    )
+  )
+
+(defn integrate [f, delta]
+  (let [f_memo (memoize f)
+        int_step_memo (memoize int_step)]
+    (fn [x]
+      (let [sign (if (neg? x) -1 1)
+            delta (* delta sign)
+            steps (quot x delta)
+            tail (mod x delta)]
+        (*
+          sign
+          (+
+            (int_step_memo int_step_memo f_memo steps delta)
+            (trapeze f_memo (- x tail) x)))
+        ))))
--- clojure/5/test/ru/nsu/ccfit/azhirov/clojure/lab5/test/core.clj
+++ clojure/5/test/ru/nsu/ccfit/azhirov/clojure/lab5/test/core.clj
@@ -2,5 +2,34 @@
   (:use [ru.nsu.ccfit.azhirov.clojure.lab5.core])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(defn float-eq [expected actual eps]
+  (and
+    (> actual (- expected eps))
+    (< actual (+ expected eps))
+    ))
+
+(letfn [(f [x] x)]
+  (deftest trapeze-test
+    (is (= 1/2 (trapeze f 0 1)))
+    (is (= (float 3/8) (trapeze f 0.5 1)))
+    (is (= (float 3/8) (trapeze f 1 0.5)))
+    ))
+
+(let [eps 0.01
+      f (integrate (fn [x] 1) 0.1)]
+  (deftest const-integration
+    (is (function? f))
+    (is (float-eq 0 (f 0) eps))
+    (is (float-eq 50 (f 50) eps))
+    (is (float-eq 51 (f 51) eps))
+    (is (float-eq -50 (f -50) eps))
+    )
+  )
+
+(let [eps 0.01
+      f (integrate (fn [x] (+ x (Math/sin x))) 0.1)]
+  (deftest complex-integration
+    (is (float-eq 0 (f 0) eps))
+    (is (= (f 1) (f -1)))
+    )
+  )
