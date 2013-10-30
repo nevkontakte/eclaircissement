0d84bfafb3eeb82e88f62a8c81ae47b84372fc46
Aleks Nevkontakte <aleks@aradmin.org.ru>
2013-10-30 12:47:36 +0700

    UPD: Fixed implication encapsulation

--- clojure/6/src/ru/nsu/ccfit/azhirov/clojure/lab6/expand.clj
+++ clojure/6/src/ru/nsu/ccfit/azhirov/clojure/lab6/expand.clj
@@ -1,7 +1,4 @@
 (ns ru.nsu.ccfit.azhirov.clojure.lab6.expand
-  (:use ru.nsu.ccfit.azhirov.clojure.lab6.follows)
-  (:use ru.nsu.ccfit.azhirov.clojure.lab6.associatives)
-  (:use ru.nsu.ccfit.azhirov.clojure.lab6.not)
   (:use ru.nsu.ccfit.azhirov.clojure.lab6.expr)
   (:use ru.nsu.ccfit.azhirov.clojure.lab6.walk-tree)
   )
@@ -9,13 +6,6 @@
 (defmulti expand-transform
           "Expand operator in terms of 'and', 'or' and 'not'."
           first)
-
-(defmethod expand-transform :expr-follows [expr]
-  "Expand implication 'a -> b' into '(not a) or b'"
-  {:pre [(implication? expr)]}
-  (let [prerequisite (expand-transform (first (args expr)))
-        consequence (expand-transform (second (args expr)))]
-    (disjunction (negation prerequisite) consequence)))
 
 (defmethod expand-transform :default [expr]
   "By default do not alter an operator, but expand it's operands."
--- clojure/6/src/ru/nsu/ccfit/azhirov/clojure/lab6/follows.clj
+++ clojure/6/src/ru/nsu/ccfit/azhirov/clojure/lab6/follows.clj
@@ -1,5 +1,9 @@
 (ns ru.nsu.ccfit.azhirov.clojure.lab6.follows
-  (:use [ru.nsu.ccfit.azhirov.clojure.lab6.expr]))
+  (:use ru.nsu.ccfit.azhirov.clojure.lab6.associatives)
+  (:use ru.nsu.ccfit.azhirov.clojure.lab6.not)
+  (:use [ru.nsu.ccfit.azhirov.clojure.lab6.expr])
+  (:use [ru.nsu.ccfit.azhirov.clojure.lab6.expand])
+  )
 
 ; Implication
 
@@ -7,11 +11,19 @@
   "Check if expr represents implication."
   {:pre [(expression? expr)]}
   (and
-    (= :expr-follows (first expr))
+    (= ::expr-follows (first expr))
     (expressions? (next expr))
     (= 2 (count (next expr)))))
 
 (defn implication [prerequisite, consequence]
   "Create implication expression."
-  (list :expr-follows prerequisite consequence))
+  (list ::expr-follows prerequisite consequence))
 
+; Expand implication
+
+(defmethod expand-transform ::expr-follows [expr]
+  "Expand implication 'a -> b' into '(not a) or b'"
+  {:pre [(implication? expr)]}
+  (let [prerequisite (expand-transform (first (args expr)))
+        consequence (expand-transform (second (args expr)))]
+    (disjunction (negation prerequisite) consequence)))
